00ed56396e925d2194ce456b28303c9635a88cc8
Tyler Hull <tylerdh.hull@gmail.com>
2024-03-28 23:12:18 -0600

    Add project files.

---
+++ RootkitCoin/Block.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace RootkitCoin
+{
+    internal class Block
+    {
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string TimeStamp { get; set; }
+        public string Hash { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public int Nonce { get; set; }
+
+        public Block(int index, string timestamp, List<Transaction> transactions, string previousHash = "")
+        {
+            this.Index = index;
+            this.PreviousHash = previousHash;
+            this.TimeStamp = timestamp;
+            this.Transactions = transactions;
+            this.Nonce = 0;
+            this.Hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.PreviousHash + this.TimeStamp + this.Transactions.ToString() + Nonce;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashBytes = SHA256.Create().ComputeHash(blockBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+
+        public void Mine(int difficulty)
+        {
+            while (this.Hash.Substring(0, difficulty) != new string('0', difficulty))
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                Console.WriteLine("Mining: " + this.Hash);
+            }
+
+            Console.WriteLine("block has been Mined: " + this.Hash);
+        }
+    }
+}
---
+++ RootkitCoin/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using EllipticCurve;
+using Newtonsoft.Json;
+
+namespace RootkitCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+            PrivateKey key3 = new PrivateKey();
+            PublicKey wallet3 = key3.publicKey();
+
+
+            Blockchain rootkitCoin = new Blockchain(3, 100);
+
+            rootkitCoin.MinePendingTransactions(wallet1);
+            rootkitCoin.MinePendingTransactions(wallet2);
+            rootkitCoin.MinePendingTransactions(wallet3);
+
+            Console.Write("\nBalance of Wallet1: $" + rootkitCoin.GetBalanceOfWallet(wallet1).ToString());
+            Console.Write("\nBalance of Wallet2: $" + rootkitCoin.GetBalanceOfWallet(wallet2).ToString());
+            Console.Write("\nBalance of Wallet3: $" + rootkitCoin.GetBalanceOfWallet(wallet3).ToString());
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 55.00m);
+            tx1.SignTransaction(key1);
+            rootkitCoin.addPendingTransaction(tx1);
+
+            Transaction tx2 = new Transaction(wallet3, wallet2, 20.00m);
+            tx2.SignTransaction(key3);
+            rootkitCoin.addPendingTransaction(tx2);
+
+            rootkitCoin.MinePendingTransactions(wallet3);
+
+            Console.Write("\nBalance of Wallet1: $" + rootkitCoin.GetBalanceOfWallet(wallet1).ToString());
+            Console.Write("\nBalance of Wallet2: $" + rootkitCoin.GetBalanceOfWallet(wallet2).ToString());
+            Console.Write("\nBalance of Wallet3: $" + rootkitCoin.GetBalanceOfWallet(wallet3).ToString());
+
+            string blockJSON = JsonConvert.SerializeObject(rootkitCoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+            if (rootkitCoin.IsChainValid())
+            { Console.WriteLine("Blockchain is Valid!!!"); }
+            else { Console.WriteLine("Blockchain is NOT valid."); }
+        }
+    }
+}
